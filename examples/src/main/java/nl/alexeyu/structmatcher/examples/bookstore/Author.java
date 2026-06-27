21f29e22ab8f5d3c0f95e3a261fce5070d103452
Oleksii Ustenko <alexeyu@gmail.com>
2026-06-27 21:38:25 +0200

    Convert some examples to records

--- examples/src/main/java/nl/alexeyu/structmatcher/examples/bookstore/Author.java
+++ examples/src/main/java/nl/alexeyu/structmatcher/examples/bookstore/Author.java
@@ -1,25 +1,4 @@
 package nl.alexeyu.structmatcher.examples.bookstore;
 
-public final class Author {
-
-    private String firstName;
-
-    private String lastName;
-
-    Author() {
-    }
-
-     Author(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
+public record Author(String firstName, String lastName) {
 }
--- examples/src/main/java/nl/alexeyu/structmatcher/examples/bookstore/Server.java
+++ examples/src/main/java/nl/alexeyu/structmatcher/examples/bookstore/Server.java
@@ -1,25 +1,4 @@
 package nl.alexeyu.structmatcher.examples.bookstore;
 
-public final class Server {
-
-    private String ip;
-
-    private int port;
-
-    Server() {
-    }
-
-    Server(String ip, int port) {
-        this.ip = ip;
-        this.port = port;
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public int getPort() {
-        return port;
-    }
-
+public record Server(String ip, int port) {
 }
